98a50a253d4d6e5003198db8b0b827e5b1f168c8
dlgeraghty <dvd.loge@gmail.com>
2021-05-16 22:57:56 +0200

    add name prompt

--- practica5/Cliente.java
+++ practica5/Cliente.java
@@ -4,15 +4,19 @@ import java.util.*;
 
 public class Cliente{
 
-	private String username;
-	private String ip;
+	private static String username;
+	private static String ip;
 
 	public static void main(String[] args) throws IOException{
+
+		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(" Cual es tu nombre de usuario " );
+		username = keyboard.readLine(); 
+
 		Socket s = new Socket("localhost", 8080);
 
 		OyenteServidor serverConn = new OyenteServidor(s);
 
-		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter out = new PrintWriter(s.getOutputStream(), true);
 
 		new Thread(serverConn).start();
