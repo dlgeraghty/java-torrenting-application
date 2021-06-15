88ba66b2e94d62ca5887ef24d3cd35458cb2669d
dlgeraghty <dvd.loge@gmail.com>
2021-06-15 22:06:43 +0200

    reestructurar clase Servidor

--- practica5/Servidor.java
+++ practica5/Servidor.java
@@ -5,21 +5,21 @@ import java.util.concurrent.*;
 
 public class Servidor{
 
-	private static ArrayList<OyenteCliente> clients = new ArrayList<>();
 	private static final int PORT = 8080;
-	private static ExecutorService pool = Executors.newFixedThreadPool(4);
 
 	public static void main(String[] args) throws IOException{
 
+		//TODO: Crear los monitores para asegurar concurrencia:
+		//TODO: MonitorDelSocket mSocket = new MonitorDelSocket();
+		//TODO: MonitorDeArchivos mArchivos = new MonitorDeArchivos();
 
 		ServerSocket listener = new ServerSocket(PORT);
 
 		while(true){
 			System.out.println(" Esperando a que se conecte un cliente...");
 			Socket client = listener.accept();
+			//TODO: Habria que pasarle los monitores tambien al OyenteCliente:
 			OyenteCliente clientThread = new OyenteCliente(client);
-			clients.add(clientThread);
-			pool.execute(clientThread);
 		}
 	}
 }
